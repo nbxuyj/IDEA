79d97dc19d7c2a2cfce27607685c626b98aab241
许要军 <xuyaojun@cdqcp.com>
2023-01-06 13:41:35 +0800

    更新。

--- guigu/src/main/java/com/xuyj/controller/HelloController.java
+++ guigu/src/main/java/com/xuyj/controller/HelloController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@Api(value = "Hello接口说明",tags = "接口说明")
+@Api(value = "Hello接口说明",tags = "guigu示例")
 public class HelloController {
     @ResponseBody
-    @ApiOperation(value = "测试hello world",notes = "提示内容")
+    @ApiOperation(value = "hello无参",notes = "这是第一个hello方法")
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello()
     {
--- guigu/src/main/java/com/xuyj/controller/HelloParam.java
+++ guigu/src/main/java/com/xuyj/controller/HelloParam.java
@@ -9,17 +9,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-@Api(value = "HelloParam接口说明", tags = "接口说明")
+@Api(value = "HelloParam接口说明", tags = "guigu示例")
 public class HelloParam {
     @ResponseBody
-    @ApiOperation(value = "Get传参", notes = "userID需要添加")
-    @RequestMapping(value="helloParam", method = RequestMethod.GET)
+    @ApiOperation(value = "helloP1传参get", notes = "userID需要传参")
+    // @RequestMapping(value="helloP1", method = RequestMethod.GET)
+    @GetMapping("helloP1")
     public String HelloParam(String userID) {
         return "Hello " + userID;
     }
 
-    @ApiOperation(value = "Post传参", notes = "food[]")
-    @RequestMapping(value = "helloP2", method = RequestMethod.POST)
+    @ApiOperation(value = "helloP2传参post", notes = "food[]")
+    @PostMapping("helloP2")
     @ResponseBody
     public Map<String, Object> HelloParam2(String name, @RequestParam(name = "food[]") String[] food) throws Exception {
         Map<String, Object> result = new HashMap<>();
