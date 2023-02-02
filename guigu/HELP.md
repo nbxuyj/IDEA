cde57e112202c385b2146dcce54fa3c67e5bf5f2
许要军 <xuyaojun@cdqcp.com>
2023-02-02 21:38:18 +0800

    调试时报无法加载主类

---
+++ guigu/HELP.md
@@ -0,0 +1,3 @@
+#2023.01.06
+swagger启动
+http://localhost:8080/swagger-ui.html#/
---
+++ guigumain/spring-boot-02-config/src/main/java/com/atguigu/springboot/SpringBoot02ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+//@ImportResource(locations = {"classpath:bean.xml"})
+//已采用配置类方式进行配置。
+
+
+@SpringBootApplication
+public class SpringBoot02ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02ConfigApplication.class, args);
+    }
+
+}
