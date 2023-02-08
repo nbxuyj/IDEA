96f2a6976dc05174236ac42d66c0f5f7ff882671
许要军 <xuyaojun@cdqcp.com>
2023-02-08 20:59:56 +0800

    更新。

---
+++ spring-boot-05-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -0,0 +1,38 @@
+package com.atguigu.springboot.config;
+
+import com.atguigu.springboot.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        super.addViewControllers(registry);
+        //浏览器发送 /atguigu
+        registry.addViewController("/atguigu").setViewName("success");
+    }
+
+    @Bean
+    //所有的WebMvcConfigurerAdapter组件都会一起起作用。
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+        return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+
+        return new MyLocaleResolver();
+    }
+}
