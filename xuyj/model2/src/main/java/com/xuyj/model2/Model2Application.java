669e3f0c3dd22baeca8591ba232388b697dd7a50
许要军 <xuyaojun@cdqcp.com>
2023-02-11 06:59:47 +0800

    创建父工程。

---
+++ xuyj/model2/src/main/java/com/xuyj/model2/Model2Application.java
@@ -0,0 +1,11 @@
+package com.xuyj.model2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Model2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Model2Application.class,args);
+    }
+}
