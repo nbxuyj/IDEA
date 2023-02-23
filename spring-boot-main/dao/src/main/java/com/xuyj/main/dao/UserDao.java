e472e3550eb1256c4a8ea5d4cbc2c6caa654a779
许要军 <xuyaojun@cdqcp.com>
2023-02-23 14:05:21 +0800

    更新。

--- spring-boot-main/dao/src/main/java/com/xuyj/main/dao/UserDao.java
+++ spring-boot-main/dao/src/main/java/com/xuyj/main/dao/UserDao.java
@@ -2,12 +2,11 @@ package com.xuyj.main.dao;
 
 
 import com.xuyj.main.pojo.User;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserDao extends CrudRepository<SecurityProperties.User,Long> {
+public interface UserDao extends CrudRepository<User,Long> {
 
     Optional<User> findByName(String name);
 }
