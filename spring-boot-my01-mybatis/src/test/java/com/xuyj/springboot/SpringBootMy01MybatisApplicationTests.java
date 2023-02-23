5e56dc093b193139c6bc02833e7b28553bd115e9
许要军 <xuyaojun@cdqcp.com>
2023-02-23 10:03:33 +0800

    未签入的，再次签入。

---
+++ spring-boot-my01-mybatis/src/test/java/com/xuyj/springboot/SpringBootMy01MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuyj.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMy01MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
