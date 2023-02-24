90f66a4526fbbf59863cb6681d2a3c034d14a2b8
许要军 <xuyaojun@cdqcp.com>
2023-02-24 13:32:47 +0800

    移除.idea版本控制，签入swagger问题。

--- spring-boot-main/controller/src/main/java/com/xuyj/main/controller/UserController.java
+++ spring-boot-main/controller/src/main/java/com/xuyj/main/controller/UserController.java
@@ -2,6 +2,8 @@ package com.xuyj.main.controller;
 
 import com.xuyj.main.pojo.User;
 import com.xuyj.main.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,11 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Api(value = "Hello接口说明",tags = "guigu示例")
 public class UserController {
 
     @Autowired
     private UserService userService;
-
+    @ApiOperation(value = "hello无参",notes = "这是第一个hello方法")
     @RequestMapping(value = "/find",method = RequestMethod.GET)
     public User findByName(String name){
         return userService.findUserByName(name);
--- spring-boot-main/readme.md
+++ spring-boot-main/readme.md
@@ -1,14 +1,5 @@
 # 2023.02.23
 主项目
-步骤1：集成项目。 完工
-参考：https://www.jianshu.com/p/f9ffba8a03d0
-
-步骤2：集成JPA 完工
-https://www.jianshu.com/p/b5d44a4ba693
-
-步骤2：集成Swagger
-
-
 
 pojo:实体模块。
 dao：数据持久模块
@@ -17,9 +8,13 @@ interface:业务接口模块
 service：业务接口实现模块
 controller:webAPI模块。
 
-搭建JPA
+需求1：集成项目【进度：完工】
+教程：https://www.jianshu.com/p/f9ffba8a03d0
 
+需求2：集成JPA 【进度：完工】
+教程： https://www.jianshu.com/p/b5d44a4ba693
 启动： http://localhost:9091/user/find?name=laowang
 
-
+需求3：整合Swagger。【进度：进行中】
+教程：https://www.jianshu.com/p/06a400b79279
  
