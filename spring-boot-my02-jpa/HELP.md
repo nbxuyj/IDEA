090f345efb9942f922c1b495174d7b0f68d2753d
许要军 <xuyaojun@cdqcp.com>
2023-02-24 13:39:00 +0800

    补充

---
+++ spring-boot-my02-jpa/HELP.md
@@ -0,0 +1,3 @@
+# 2023.02.23
+参考：
+https://www.jianshu.com/p/b5d44a4ba693
---
+++ spring-boot-my02-jpa/src/main/java/com/xuyj/springboot/SpringBootMy02JpaApplication.java
@@ -0,0 +1,13 @@
+package com.xuyj.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMy02JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMy02JpaApplication.class, args);
+    }
+
+}
