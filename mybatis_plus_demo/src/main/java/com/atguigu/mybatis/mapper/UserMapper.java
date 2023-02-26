158feccb0ddcc83d223220d193bf5690780b5392
许要军 <xuyaojun@cdqcp.com>
2023-02-26 22:31:58 +0800

    @Repository 表示持久层。不报红。

--- mybatis_plus_demo/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ mybatis_plus_demo/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.atguigu.mybatis.mapper;
 
 import com.atguigu.mybatis.pojo.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 }
