ca373a53a72c39ca00d061ef9d138a951e80821e
许要军 <xuyaojun@cdqcp.com>
2023-02-27 16:22:49 +0800

    由于UserServiceImpl中未加@Service注解。

--- mybatis_plus_demo/src/main/java/com/atguigu/mybatis/service/impl/UserServiceImpl.java
+++ mybatis_plus_demo/src/main/java/com/atguigu/mybatis/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.atguigu.mybatis.mapper.UserMapper;
 import com.atguigu.mybatis.pojo.User;
 import com.atguigu.mybatis.service.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 }
