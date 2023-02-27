f372078928ef1461a6b576a97c4751e8b4245470
许要军 <xuyaojun@cdqcp.com>
2023-02-27 15:55:01 +0800

    支持guid.

--- mybatis_plus_demo/src/main/java/com/atguigu/mybatis/pojo/User.java
+++ mybatis_plus_demo/src/main/java/com/atguigu/mybatis/pojo/User.java
@@ -6,7 +6,7 @@ import lombok.*;
 
 @Data
 public class User {
-    //@TableId("uid")
+    //@TableId("uid") ASSIGN_ID 雪花算法 ASSIGN_UUID GUID
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private Long id;
     private String name;
