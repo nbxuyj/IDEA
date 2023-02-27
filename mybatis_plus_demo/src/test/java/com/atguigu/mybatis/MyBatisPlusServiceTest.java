6bba9f226bc9edc977787e3013c8790b05eae895
许要军 <xuyaojun@cdqcp.com>
2023-02-27 16:30:39 +0800

    1.通过TableName解决问题。 2.通过全局配置解决问题。

--- mybatis_plus_demo/src/test/java/com/atguigu/mybatis/MyBatisPlusServiceTest.java
+++ mybatis_plus_demo/src/test/java/com/atguigu/mybatis/MyBatisPlusServiceTest.java
@@ -1,9 +1,12 @@
 package com.atguigu.mybatis;
 
+import com.atguigu.mybatis.pojo.User;
 import com.atguigu.mybatis.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 public class MyBatisPlusServiceTest {
@@ -12,9 +15,24 @@ public class MyBatisPlusServiceTest {
     private UserService userService;
 
     @Test
-    public void testGetCount(){
+    public void testGetCount() {
         long count = userService.count();
         System.out.println("总记录数：" + count);
     }
+
+    @Test
+    public void testSaveBatch() {
+        // SQL长度有限制，海量数据插入单条SQL无法实行，
+        // 因此MP将批量插入放在了通用Service中实现，而不是通用Mapper
+        ArrayList<User> users = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            User user = new User();
+            user.setName("ybc" + i);
+            user.setAge(20 + i);
+            users.add(user);
+        }
+        userService.saveBatch(users);
+    }
+
 
 }
