b735da17bf206f0ff5906b74f5e0d67174a4568b
许要军 <xuyaojun@cdqcp.com>
2023-03-02 22:05:41 +0800

    增加新项目mybatis_datasource测试双数据源。

---
+++ mybatis_plus_datasource/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.mybatis.mapper;
+
+import com.atguigu.mybatis.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author atguigu
+ * @since 2023-03-02
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
