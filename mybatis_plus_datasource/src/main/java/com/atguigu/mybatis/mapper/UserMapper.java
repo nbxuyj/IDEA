5bebe98605c060d06e2d2e7278997c5162e7ce3d
许要军 <xuyaojun@cdqcp.com>
2023-03-04 08:59:05 +0800

    下一步测试代码生成器功能。

--- mybatis_plus_datasource/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ mybatis_plus_datasource/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.atguigu.mybatis.mapper;
 
 import com.atguigu.mybatis.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author atguigu
  * @since 2023-03-02
  */
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 
 }
