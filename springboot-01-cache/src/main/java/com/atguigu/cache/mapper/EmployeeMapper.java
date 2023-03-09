5225dd0302ec244277ffb1ae828b34076c4ff10e
许要军 <xuyaojun@cdqcp.com>
2023-03-09 22:45:48 +0800

    更新，问题未发现

--- springboot-01-cache/src/main/java/com/atguigu/cache/mapper/EmployeeMapper.java
+++ springboot-01-cache/src/main/java/com/atguigu/cache/mapper/EmployeeMapper.java
@@ -9,15 +9,15 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface EmployeeMapper {
     @Select("select * from employee where id=#{id}")
-    public Employee getEmpById(Integer id);
+    Employee getEmpById(Integer id);
 
     @Update("upate employee set lastName=#{lastName},email=#{email},gender=#{gender}, dId={#dId} where id=#{id}")
-    public  void updateEmp(Employee employee);
+     void updateEmp(Employee employee);
 
     @Delete("delete employee where id=#{id}")
-    public void deleteEmpById(Integer id);
+    void deleteEmpById(Integer id);
 
     @Insert("insert into employee(lastName,email,gender,d_id) values(#{lastName},#{email},#{gender},#{d_id})")
-    public void insertEmployee(Employee employee);
+    void insertEmployee(Employee employee);
 
 }
--- springboot-01-cache/src/test/java/com/atguigu/cache/Springboot01CacheApplicationTests.java
+++ springboot-01-cache/src/test/java/com/atguigu/cache/Springboot01CacheApplicationTests.java
@@ -5,6 +5,8 @@ import com.atguigu.cache.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
 class Springboot01CacheApplicationTests {
