2f6370e114dc1aef2c92215cf976c16feb7e9bab
许要军 <xuyaojun@cdqcp.com>
2023-03-11 07:51:25 +0800

    上一问题答复： 1.语句错误update、 #{dId} 、 d_id=#{dId} where id=#{id}" 语句写错。 2.传参时/emp?错误

--- springboot-01-cache/src/main/java/com/atguigu/cache/mapper/EmployeeMapper.java
+++ springboot-01-cache/src/main/java/com/atguigu/cache/mapper/EmployeeMapper.java
@@ -11,7 +11,7 @@ public interface EmployeeMapper {
     @Select("select * from employee where id=#{id}")
     Employee getEmpById(Integer id);
 
-    @Update("upate employee set lastName=#{lastName},email=#{email},gender=#{gender}, dId={#dId} where id=#{id}")
+    @Update("update employee set lastName=#{lastName},email=#{email},gender=#{gender}, d_id=#{dId} where id=#{id}")
      void updateEmp(Employee employee);
 
     @Delete("delete employee where id=#{id}")
