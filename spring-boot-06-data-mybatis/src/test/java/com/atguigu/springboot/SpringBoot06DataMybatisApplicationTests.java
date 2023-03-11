39f5c2a39a1f4e78752ebed9914dcfab50c4333d
许要军 <xuyaojun@cdqcp.com>
2023-03-11 09:54:32 +0800

    上一问题答复：请求方式错误。

--- spring-boot-06-data-mybatis/src/test/java/com/atguigu/springboot/SpringBoot06DataMybatisApplicationTests.java
+++ spring-boot-06-data-mybatis/src/test/java/com/atguigu/springboot/SpringBoot06DataMybatisApplicationTests.java
@@ -1,13 +1,22 @@
 package com.atguigu.springboot;
-
+import com.atguigu.springboot.bean.*;
+import com.atguigu.springboot.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.parsing.EmptyReaderEventListener;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBoot06DataMybatisApplicationTests {
 
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+
     @Test
     void contextLoads() {
+        Employee emp = employeeMapper.getEmpById(1);
+        System.out.println(emp.getId());
     }
 
 }
