8258e4ceb5d8a7e38d214d92c6e6cef62f0449b8
许要军 <xuyaojun@cdqcp.com>
2023-03-13 14:40:49 +0800

    测试返回OK。

--- platform/platform-manage-api/src/main/java/com/xuyj/platform/manage/api/controller/DepartmentController.java
+++ platform/platform-manage-api/src/main/java/com/xuyj/platform/manage/api/controller/DepartmentController.java
@@ -3,14 +3,13 @@ package com.xuyj.platform.manage.api.controller;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xuyj.plateform.common.entity.ResultMessage;
 import com.xuyj.platform.db.entity.Department;
+import com.xuyj.platform.manage.api.entity.DepListParam;
 import com.xuyj.platform.service.DepartmentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -22,9 +21,14 @@ public class DepartmentController {
     DepartmentService departmentService;
 
     @ApiOperation("获取列表")
-    @GetMapping("/list")
+    @PostMapping("/list")
+    @ResponseBody
     //分页查询所有数据
-    public Page<Department> getDepartmentList(@ApiParam(value = "部门分页", required = true) Page<Department> page, @ApiParam(value = "部门查询条件", required = true) Department dep) {
+    public Page<Department> getDepartmentList(@RequestBody @ApiParam(value = "参数类", required = true) DepListParam p) {
+
+        Page<Department> page = new Page<>(p.getPageNumber(), p.getPageSize());
+        Department dep = new Department();
+        dep.setDepartmentname(p.getDepName());
         Page<Department> page1 = departmentService.mySelectAll(page, dep);
         return page1;
     }
---
+++ platform/platform-manage-api/src/main/java/com/xuyj/platform/manage/api/entity/DepListParam.java
@@ -0,0 +1,9 @@
+package com.xuyj.platform.manage.api.entity;
+
+import lombok.Data;
+
+@Data
+public class DepListParam extends PageRequest {
+
+    private  String depName;
+}
