7fe98e083f6d0b3aeb5072611cb87942ac8f8b09
许要军 <xuyaojun@cdqcp.com>
2023-09-24 20:19:26 +0800

    中介者模式

---
+++ JavaEE/demospring/src/main/java/com/xuyj/demospring/designMode/mediator/Mediator.java
@@ -0,0 +1,9 @@
+package com.xuyj.demospring.designMode.mediator;
+
+/**
+ * 抽象中介者
+ */
+public  abstract  class Mediator {
+    public abstract void register(Colleague colleague);
+    public abstract void relay(Colleague cl); //转发
+}
